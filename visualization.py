41a13f5341bed7a7aac1e32b2f99635925be5b43
junghyey <junghyey@turing.cs.grinnell.edu>
2024-12-17 18:33:07 -0600

    comment out gif save

--- visualization.py
+++ visualization.py
@@ -77,7 +77,7 @@ def main():
     plt.show()
 
     # Save the animation as a GIF
-    ani.save("visualization_outputs/visualization_extreme_1.gif", writer=PillowWriter(fps=30))
+    #ani.save("visualization_outputs/visualization_extreme_1.gif", writer=PillowWriter(fps=30))
     
 if __name__ == "__main__":
     main()
